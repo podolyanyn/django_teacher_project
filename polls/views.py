ae97a3fee7f6599749474e0d5355fbb594432f0e
igor <igor@example.com>
2023-03-04 16:05:52 +0200

    Lesson 45. Add tests.

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.http import Http404
 from django.urls import reverse
 from django.views import generic
 from .forms import QuestionForm
+from django.utils import timezone
 
 def hello(request):
     return HttpResponse("Hello, world !")
@@ -16,12 +17,19 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
